ac42bcc7ca3fd52ba9a37ebbd9676212d252b1bd
philip fraser <pr.fraser3@gmail.com>
2017-09-07 15:15:13 +1000

    asdfasdf

--- app/assets/javascripts/ie-detect.js
+++ app/assets/javascripts/ie-detect.js
@@ -1,7 +1,7 @@
-if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
+$(function(){if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
 {
   alert("Please dont use IE.");
   document.body.className = "iestyle";
   // document.getElementById("bodyid").className = "iestyle";
   // $('body').addClass('iestyle');
-}
+}})
