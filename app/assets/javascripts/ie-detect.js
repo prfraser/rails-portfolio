b4c13a0559e82ce15fd230c076b681d7752ebc91
philip fraser <pr.fraser3@gmail.com>
2017-09-07 14:04:24 +1000

    added js to detect ie

--- app/assets/javascripts/ie-detect.js
+++ app/assets/javascripts/ie-detect.js
@@ -1,4 +1,4 @@
 if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
 {
-  alert("Please dont use IE.");
+  document.getElementById("body").classList.add('iestyle');
 }
