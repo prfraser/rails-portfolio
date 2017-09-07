cbde6e3b33c148736c94b05238b10c8cb14a5b2b
philip fraser <pr.fraser3@gmail.com>
2017-09-07 14:10:12 +1000

    changed id on body

--- app/assets/javascripts/ie-detect.js
+++ app/assets/javascripts/ie-detect.js
@@ -1,4 +1,4 @@
 if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
 {
-  document.getElementById("body").classList.add('iestyle');
+  document.getElementById("bodyid").classList.add('iestyle');
 }
