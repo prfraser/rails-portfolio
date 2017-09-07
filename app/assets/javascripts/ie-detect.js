d267954bd5eb2fe60f3db06fbeb74a10eb272e15
philip fraser <pr.fraser3@gmail.com>
2017-09-07 15:02:04 +1000

    asdfasdf

--- app/assets/javascripts/ie-detect.js
+++ app/assets/javascripts/ie-detect.js
@@ -1,5 +1,6 @@
 if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
 {
   alert("Please dont use IE.");
-  document.body.className = "iestyle";
+  // document.body.className = "iestyle";
+  document.getElementById("bodyid").className = "iestyle";
 }
