cdfd24ad86ef290fe8407eeca2ef338074fab5b1
philip fraser <pr.fraser3@gmail.com>
2017-09-07 15:25:13 +1000

    asdfasdf

--- app/assets/javascripts/ie-detect.js
+++ app/assets/javascripts/ie-detect.js
@@ -1,4 +1,9 @@
-$(function(){if (navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1))
-{
-  document.body.className = "iestyle";
-}})
+$(document).on('turbolinks:load', function () {
+  if (
+    navigator.appName == 'Microsoft Internet Explorer'
+    || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/))
+    || (typeof $.browser !== "undefined" && $.browser.msie == 1)
+  ) {
+    document.body.className = "iestyle";
+  }
+});
